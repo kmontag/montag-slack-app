8137b27e8488e44d9d4bff981991702c1a7d63e8
Kevin Montag <kmontag@cs.stanford.edu>
2017-01-20 16:29:34 +0100

    Initial app implementation

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var express = require('express');
+var express  = require('express');
+var d3Format = require('d3-format');
+var request  = require('request');
+var cheerio  = require('cheerio');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,8 +13,33 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res, next) {
+  // http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+  var min = 1, max = 10;
+  var page = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  var format = d3Format.format('02');
+
+  var url = "http://textgifs.de/montag/montag" + format(page) + '.htm'
+
+  request({url: url}, function(err, response, body) {
+    if (err) {
+      next(new Error(err));
+    } else {
+      var $ = cheerio.load(body);
+      var images = $('img').filter(function() {
+        return $(this).attr('src').startsWith('montag');
+      });
+
+      var src = images.eq(Math.floor(Math.random() * images.length)).attr('src');
+      res.json({
+        attachments: [{
+          fallback: 'montag',
+          image_url: ("http://textgifs.de/montag/" + images.first().attr('src')),
+        }],
+      });
+    }
+  });
 });
 
 app.listen(app.get('port'), function() {
